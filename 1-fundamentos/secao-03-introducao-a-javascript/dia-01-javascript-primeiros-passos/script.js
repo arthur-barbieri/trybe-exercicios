009c3b98484277f9bc8f7eacd951d8c1e85a7323
Arthur Claudio Barbieri Silva <arthurclaudiobarbieri@gmail.com>
2023-05-02 14:03:43 -0300

    Exercícios 2 - Tipos Primitivos, tipagem dinâmica e operações aritméticas - Concluído!

--- 1-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ 1-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,3 +1,4 @@
+// Exercício 1 - Variáveis
 let myName = 'Arthur'
 
 let birthCity = 'Jundiaí'
@@ -7,3 +8,17 @@ let birthYear = 1993
 console.log(myName)
 console.log(birthCity)
 console.log(birthYear)
+
+// Exercício 2 - Tipos Primitivos, tipagem dinâmica e operações aritméticas
+
+let base = 5;
+
+let height = 8;
+
+let area = base * height
+console.log(area)
+
+let perimeter = base + base + height + height
+console.log(perimeter)
+
+// Exercício 3 - Condições if e else
