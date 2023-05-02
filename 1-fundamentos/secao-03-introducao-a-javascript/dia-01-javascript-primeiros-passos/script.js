021786a8f450709916670b4d68b9ec4ac2d6b718
Arthur Claudio Barbieri Silva <arthurclaudiobarbieri@gmail.com>
2023-05-02 15:29:16 -0300

    Exercício 3 - Condições if e else - Concluído!

--- 1-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ 1-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -22,3 +22,15 @@ let perimeter = base + base + height + height
 console.log(perimeter)
 
 // Exercício 3 - Condições if e else
+
+let nota = 93;
+
+if (nota >= 80) {
+    console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!')
+} else if (nota < 80 && nota >= 60) {
+    console.log('Você está na nossa lista de espera.')
+} else if (nota < 60) {
+    console.log('Infelizmente, você reprovou.')
+}
+
+// Exercício 4
