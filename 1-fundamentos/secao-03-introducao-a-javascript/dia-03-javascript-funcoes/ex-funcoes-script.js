5f67aa3ac2b3c872875a01d4fa4284208d4af291
Arthur Claudio Barbieri Silva <arthurclaudiobarbieri@gmail.com>
2023-05-04 15:47:29 -0300

    Exercício: Funções - Concluído!

---
+++ 1-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/ex-funcoes-script.js
@@ -0,0 +1,23 @@
+let saldo = 500
+
+function somar(valor) {
+    return saldo + valor
+}
+
+function sub(valor) {
+    return saldo - valor
+}
+
+function mult(valor) {
+    return saldo * valor
+}
+
+function div(valor) {
+    return saldo / valor
+}
+
+console.log(somar(10))
+console.log(sub(10))
+console.log(mult(10))
+console.log(div(10))
+
