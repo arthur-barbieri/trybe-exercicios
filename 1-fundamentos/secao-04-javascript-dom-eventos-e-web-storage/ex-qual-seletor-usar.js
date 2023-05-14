ba2893c33b191e45b73ad3f6b675be03bbed1b51
Arthur Claudio Barbieri Silva <arthurclaudiobarbieri@gmail.com>
2023-05-14 19:34:48 -0300

    Exercícios antes da aula Ao Vivo Concluídos!

---
+++ 1-fundamentos/secao-04-javascript-dom-eventos-e-web-storage/ex-qual-seletor-usar.js
@@ -0,0 +1,23 @@
+document.body.style.backgroundColor = '#f3f3f3'
+
+document.getElementsByTagName('h1')[0].style.backgroundColor = '#00b069'
+
+document.getElementsByTagName('header')[0].style.padding = '0px'
+
+document.getElementsByTagName('h1')[0].style.height = '60px'
+
+document.getElementsByTagName('h1')[0].style.fontSize = '25px'
+
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = '#ff9f84'
+
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = '#f9db5e'
+
+document.querySelector('.urgente1').style.backgroundColor = '#a500f3'
+
+document.querySelector('.urgente2').style.backgroundColor = '#a500f3'
+
+document.querySelector('.naoUrgente1').style.backgroundColor = '#232525'
+
+document.querySelector('.naoUrgente2').style.backgroundColor = '#232525'
+
+document.getElementById('footer-container').style.backgroundColor = '#003533'
